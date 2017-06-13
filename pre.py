3836e309a7e07314ebc31f89986f1ccc701a0eb8
Sasikala Mani <mani.37@ruan.cse.ohio-state.edu>
2017-06-13 11:36:07 -0400

    first train

--- pre.py
+++ pre.py
@@ -1,6 +1,10 @@
 import nltk
 from collections import Counter
 from nltk.corpus import stopwords
+import theano
+import theano.tensor as T
+import numpy as np
+import random
 
 #read from text file
 file = open("imdb_labelled.txt", "r")
@@ -38,19 +42,64 @@ outputs = [0] * rows
 
 
 file1 = open("imdb_labelled.txt", "r")
-lineNum = 0
-for line in file1:
-    token = nltk.word_tokenize(line.decode("utf8"))
-    lineNum +=1 
-    y = token[len(token)-1]
-    outputs[lineNum-1] = y
-    token.pop()
-    for word in token:
-        lookup = index[word.lower()]
-        matrix[lineNum-1][lookup]= 1
+def preproc(file1):
+    lineNum = 0
+    for line in file1:
+        token = nltk.word_tokenize(line.decode("utf8"))
+        lineNum +=1 
+        y = token[len(token)-1]
+        outputs[lineNum-1] = [int(y)]
+        token.pop()
+        for word in token:
+            lookup = index[word.lower()]
+            matrix[lineNum-1][lookup]= 1
 
-#matrix with binary equivalent of data
-#print(matrix)
-print(outputs)
+    #matrix with binary equivalent of data
+    #print(matrix)
+    return (matrix, outputs)
+    #return outputs
+(array, outputs1) = preproc(file1)
+
+
+X = theano.shared(value=np.asarray(array), name='X')
+y = theano.shared(value=np.asarray(outputs1), name='y')
+
+rng = np.random.RandomState(1234)
+LEARNING_RATE = 0.01
+ 
+def layer(n_in, n_out):
+    return theano.shared(value=np.asarray(rng.uniform(low=-1.0, high=1.0, 
+        size=(n_in, n_out)), dtype=theano.config.floatX), name='W', borrow=True)
+ 
+W1 = layer(3145, 7)
+W2 = layer(7, 1)
+ 
+output = T.nnet.sigmoid(T.dot(T.nnet.sigmoid(T.dot(X, W1)), W2))
+#output1 = T.nnet.sigmoid(T.dot(T.nnet.sigmoid(T.dot(X1, W1)), W2))
+cost = T.sum((y - output) ** 2)
+updates = [(W1, W1 - LEARNING_RATE * T.grad(cost, W1)), (W2, W2 - LEARNING_RATE * T.grad(cost, W2))]
+ 
+train = theano.function(inputs=[], outputs=[], updates=updates)
+test = theano.function(inputs=[], outputs=[output])
+    #[output1])
+ 
+for i in range(1000):
+    if (i+1) % 10 == 0:
+        print(i+1)
+    train()
+ 
+new = (test()[0].tolist())
+for i in range(1000):
+    if (new[i][0] >= 0.5): 
+        new[i][0] = 1
+    else:
+        new[i][0] = 0
+
+counter = 0
+for i in range(1000):
+    if(new[i][0] == outputs1[i][0]):
+        counter= counter +1
+print(counter)
+
 
 
